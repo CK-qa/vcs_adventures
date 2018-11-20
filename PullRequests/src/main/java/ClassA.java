69bc938af599271abdb853161ca68a4ace9a76b0
Victoria Bozhko <viktoria.bozhko@jetbrains.com>
2018-11-20 19:03:34 +0300

    Reformatted code

--- PullRequests/src/main/java/ClassA.java
+++ PullRequests/src/main/java/ClassA.java
@@ -1,23 +1,21 @@
 public class ClassA {
-
-    String one;
-    String two;
-
-
-
-    public String getOne() {
-        return one;
-    }
-
-    public void setOne(String one) {
-        this.one = one;
-    }
-
-    public String getTwo() {
-        return two;
-    }
-
-    public void setTwo(String two) {
-        this.two = two;
-    }
+	
+	String one;
+	String two;
+	
+	public String getOne() {
+		return one;
+	}
+	
+	public void setOne(String one) {
+		this.one = one;
+	}
+	
+	public String getTwo() {
+		return two;
+	}
+	
+	public void setTwo(String two) {
+		this.two = two;
+	}
 }
